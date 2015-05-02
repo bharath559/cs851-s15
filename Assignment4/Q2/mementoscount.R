bb45f404df45bc73ce092aa7144d5df3fab7c1fd
bharath559 <kbharath00@gmail.com>
2015-05-02 04:07:25 -0400

    Added Graphs

--- Assignment4/Q2/mementoscount.R
+++ Assignment4/Q2/mementoscount.R
@@ -1,11 +1,16 @@
+
+
 library(RColorBrewer)
-data <- read.table("count.txt")
-finalData = rep(data[,1])
+
+myData <- read.table("count.txt")
+fullData = rep(myData[,1])
+
 title <- "CDF of Number of Mementos per URI"
 xlab <- "Number of Mementos"
 ylab <- "Probability"
-pdf("mementoscounts.pdf", height=4.0, width=4.5)
+
+pdf("memento_counts.pdf", height=4.0, width=4.5)
 par(mar=c(4,4,2.5,2) + 0.1)
 set.seed(1)
-mp <- plot(finalData, 1:length(finalData), type="l", col="red", main=title, xlab=xlab, ylab=ylab, yaxt="n")
-axis(side=2, at=seq(1,2735,length.out=5), labels=seq(0,1,by=0.1))
+mp <- plot(fullData, 1:length(fullData), type="l", col="red", main=title, xlab=xlab, ylab=ylab, yaxt="n")
+axis(side=2, at=seq(1,2735,length.out=5), labels=seq(0.2,1,by=0.2))
